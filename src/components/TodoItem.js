aa938fe81c61d10b4142e3ee469760e0683677bf
Harriet Oteng <harriet.oteng@gmail.com>
2023-05-02 06:49:43 +0000

    Add functions to add and delete tasks

---
+++ src/components/TodoItem.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+
+const TodoItem = ({ itemProp, handleChange, delTodo }) => (
+  <li>
+    <input
+      type="checkbox"
+      checked={itemProp.completed}
+      onChange={() => handleChange(itemProp.id)}
+    />
+    <button type="button" onClick={() => delTodo(itemProp.id)}>Delete</button>
+    {itemProp.title}
+  </li>
+);
+export default TodoItem;
+
+TodoItem.propTypes = {
+  itemProp: PropTypes.string.isRequired,
+};
+
+TodoItem.propTypes = {
+  itemProp: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired,
+  }).isRequired,
+  handleChange: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired,
+};
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,25 +1,30 @@
 import PropTypes from 'prop-types';
+import TodoItem from './TodoItem';
 
 const TodosList = (props) => {
-  const { todosProps } = props;
+  const { todosProps, handleChange, delTodo } = props;
   return (
     <ul>
       {todosProps.map((todo) => (
-        <li>{todo.title}</li>
+        <TodoItem
+          key={todo.id}
+          itemProp={todo}
+          handleChange={handleChange}
+          delTodo={delTodo}
+        />
       ))}
     </ul>
   );
 };
-// TodosList.propTypes = {
-//   todosProps: PropTypes.array.isRequired,
-// };
 
 TodosList.propTypes = {
   todosProps: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.number.isRequired,
+    id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     completed: PropTypes.bool.isRequired,
   })).isRequired,
+  handleChange: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired,
 };
 
 export default TodosList;
