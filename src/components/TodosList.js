a01b56004059db3ddfeb86e53f39f20ba0e49e6a
Harriet Oteng <harriet.oteng@gmail.com>
2023-05-02 08:44:13 +0000

    Add editing functionality

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -2,7 +2,9 @@ import PropTypes from 'prop-types';
 import TodoItem from './TodoItem';
 
 const TodosList = (props) => {
-  const { todosProps, handleChange, delTodo } = props;
+  const {
+    todosProps, handleChange, delTodo, setUpdate,
+  } = props;
   return (
     <ul>
       {todosProps.map((todo) => (
@@ -11,6 +13,7 @@ const TodosList = (props) => {
           itemProp={todo}
           handleChange={handleChange}
           delTodo={delTodo}
+          setUpdate={setUpdate}
         />
       ))}
     </ul>
@@ -25,6 +28,7 @@ TodosList.propTypes = {
   })).isRequired,
   handleChange: PropTypes.func.isRequired,
   delTodo: PropTypes.func.isRequired,
+  setUpdate: PropTypes.func.isRequired,
 };
 
 export default TodosList;
--- src/components/TodosLogic.js
+++ src/components/TodosLogic.js
@@ -49,10 +49,28 @@ const TodosLogic = () => {
     setTodos([...todos, newTodo]);
   };
 
+  const setUpdate = (updatedTitle, id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          /* eslint-disable no-param-reassign */
+          todo.title = updatedTitle;
+          // {...todo, todo.title = updatedTitle}
+        }
+        return todo;
+      }),
+    );
+  };
+
   return (
     <div>
       <InputTodo addTodoItem={addTodoItem} />
-      <TodosList todosProps={todos} handleChange={handleChange} delTodo={delTodo} />
+      <TodosList
+        todosProps={todos}
+        handleChange={handleChange}
+        delTodo={delTodo}
+        setUpdate={setUpdate}
+      />
     </div>
   );
 };
