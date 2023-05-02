9a4b2fdf3932bc35b4e8b3cff59e514f9c969aa5
Harriet Oteng <harriet.oteng@gmail.com>
2023-05-02 15:06:57 +0000

    Fix linter errors

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -15,8 +15,8 @@ const TodoItem = ({
   const handleEditing = () => {
     setEditing(true);
   };
-  let viewMode = {};
-  let editMode = {};
+  const viewMode = {};
+  const editMode = {};
   if (editing) {
     viewMode.display = 'none';
   } else {
